5e02afa6cb4d30fa4a042a11bf7357bbee35fba3
AbdolClick <rezarst19@gmail.com>
2026-07-14 20:47:45 +0400

    feat(core): create Atlas Kernel

---
+++ web/src/core/kernel/AtlasKernel.ts
@@ -0,0 +1,13 @@
+export class AtlasKernel {
+  constructor() {
+    console.log("Atlas Kernel initialized");
+  }
+
+  boot() {
+    console.log("Booting Atlas...");
+  }
+
+  shutdown() {
+    console.log("Shutting down Atlas...");
+  }
+}
